5ecf58a3646ac1be92ff70988a69b917e2c82430
hosenA51 <hosenahmed2552@gmail.com>
2024-10-22 12:24:59 +0600

    add time when marked as read

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
 import Bookmark from "../Bookmark/Bookmark";
 
-const Bookmarks = ({bookmarks}) => {
+const Bookmarks = ({bookmarks, readingTime}) => {
     return (
         <div className="md:w-1/3 bg-gray-300 ml-4 mt-2 pt-4">
-            <h2 className="text-3xl text-center">Bookmarked Blogs: {bookmarks.length}</h2>
+            <div>
+                <h3 className="text-3xl m-4 bg-slate-200 p-4 rounded-xl">Spent time on Read: {readingTime}</h3>
+            </div>
+            <h2 className="text-2xl text-center">Bookmarked Blogs: {bookmarks.length}</h2>
             {
                 bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
             }
@@ -13,7 +16,8 @@ const Bookmarks = ({bookmarks}) => {
 };
 
 Bookmarks.propTypes = {
-    bookmarks: PropTypes.array
+    bookmarks: PropTypes.array,
+    readingTime: PropTypes.number
 }
 
 export default Bookmarks;
